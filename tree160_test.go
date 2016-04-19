63a94cefde0dd597fd907415c50320beb08028bf
Alex Sergeyev <asergeyev@dyn.com>
2016-04-18 20:27:56 -0400

    Made things simpler in basic test

--- tree160_test.go
+++ tree160_test.go
@@ -13,23 +13,6 @@ type testCaseElement struct {
 	result string
 }
 
-func (s *testCaseElement) getKey() []uint32 {
-	key := make([]uint32, 0, (len(s.key)+3)/4)
-	for i := 0; i < len(s.key); i += 4 {
-		switch len(s.key) - i {
-		case 1:
-			key = append(key, uint32(s.key[i])<<24)
-		case 2:
-			key = append(key, uint32(s.key[i])<<24|uint32(s.key[i+1])<<16)
-		case 3:
-			key = append(key, uint32(s.key[i])<<24|uint32(s.key[i+1])<<16|uint32(s.key[i+2])<<8)
-		default:
-			key = append(key, uint32(s.key[i])<<24|uint32(s.key[i+1])<<16|uint32(s.key[i+2])<<8|uint32(s.key[i+3]))
-		}
-	}
-	return key
-}
-
 var testCases = [][]testCaseElement{
 	{
 		{[]byte{1, 2, 3, 0}, 24, true, "root=1.2.3.0/24 (no subtree)\\n"},
@@ -47,7 +30,7 @@ func TestTreeAppend(t *testing.T) {
 		buf := bytes.NewBuffer(nil)
 		DEBUG = buf
 		for _, s := range testcase {
-			T.addRoute(s.getKey(), s.ln, nil, s.repl)
+			T.addRoute(iptou(s.key, s.ln), s.ln, nil, s.repl)
 			got := strings.Replace(buf.String(), "\n", "\\n", -1)
 			if got != s.result {
 				t.Error(got, "!=", s.result)
@@ -59,13 +42,13 @@ func TestTreeAppend(t *testing.T) {
 	// should find exact and not-exact matches
 	for _, testcase := range testCases {
 		for _, s := range testcase {
-			exact, match, _ := T.findBestMatch(s.getKey(), s.ln)
+			exact, match, _ := T.findBestMatch(iptou(s.key, s.ln), s.ln)
 			if !exact {
-				t.Errorf("Incorrect match found for exact search, got %v key while looking for %v", match, s.getKey())
+				t.Errorf("Incorrect match found for exact search, got %v key while looking for %v", match, s)
 			}
-			exact, match, _ = T.findBestMatch(s.getKey(), s.ln+1)
+			exact, match, _ = T.findBestMatch(iptou(s.key, s.ln), s.ln+1)
 			if exact || match.prefixlen != s.ln {
-				t.Errorf("Incorrect match found for not-exact search, got %v key while looking for %v", match, s.getKey())
+				t.Errorf("Incorrect match found for not-exact search, got %v key while looking for %v", match, s)
 			}
 		}
 	}
